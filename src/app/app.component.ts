691b0cf11e0cc4cacbaadbbbbd6aadc5a0ba1727
sourav santra <souravsantra59580@gmail.com>
2020-10-15 11:55:26 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {} from './components/todos/todos.component'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crash-todolist';
+  constructor(){
+    console.warn(123)
+  }
 }
